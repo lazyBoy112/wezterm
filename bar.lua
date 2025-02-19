4bef538335c880ac167047e31df54a4cbefba37a
sanglv <lvsang2002@gmail.com>
2025-02-19 11:03:03 +0700

    fix tab size

--- bar.lua
+++ bar.lua
@@ -50,11 +50,15 @@ function M.setup()
   wezterm.on(
     'format-tab-title',
     function(tab, tabs, panes, config, hover, max_width)
-      -- local cols = panes[1].width
-      local max_w_tab = require'config'.options.tab_max_width - 4
+      local max_w_tab = require'config'.options.tab_max_width - 2
+      local width = tab.active_pane.width
+      local num_tab = #tabs
+      local min_w_tab = math.floor(width / num_tab) - 4
       local title = tab_title(tab)
       title = string.gsub(title, '%.exe$', '')
-      if title:len() > max_w_tab then
+      if min_w_tab < max_w_tab then
+        title = title:sub(1, title:len() - (title:len() - min_w_tab))
+      elseif title:len() > max_w_tab then
         title = title:sub(1, title:len() - (title:len() - max_w_tab))
       else
         title = title..string.rep(' ', max_w_tab - title:len())
@@ -69,15 +73,15 @@ function M.setup()
 
       if tab.is_active then
         bar = add_element(bar, bg_bar, bg_active, left_bar_icon, {Intensity="Normal"})
-        bar = add_element(bar, bg_active, fg_active, ' '..title, {Intensity="Bold"})
+        bar = add_element(bar, bg_active, fg_active, title, {Intensity="Bold"})
         bar = add_element(bar, bg_bar, bg_active, right_bar_icon, {Intensity="Normal"})
       elseif hover then
         bar = add_element(bar, bg_bar, fg_inactive, left_bar_icon, {Intensity="Normal"})
-        bar = add_element(bar, fg_inactive, bg_inactive, ' '..title..' ', {Intensity="Bold"})
+        bar = add_element(bar, fg_inactive, bg_inactive, title, {Intensity="Bold"})
         bar = add_element(bar, bg_bar, fg_inactive, right_bar_icon, {Intensity="Normal"})
       else
         bar = add_element(bar, bg_bar, bg_inactive, left_bar_icon, {Intensity="Normal"})
-        bar = add_element(bar, bg_inactive, fg_inactive, ' '..title..' ', {Intensity="Normal"})
+        bar = add_element(bar, bg_inactive, fg_inactive, title, {Intensity="Normal"})
         bar = add_element(bar, bg_bar, bg_inactive, right_bar_icon, {Intensity="Normal"})
       end
       return bar
